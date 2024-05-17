07939a5c135ee6de81943fc26aefde5f729807f4
sotnasnonnel <90616389+sotnasnonnel@users.noreply.github.com>
2024-05-17 17:12:57 -0300

    Update dashbi.py

--- dashbi.py
+++ dashbi.py
@@ -1,6 +1,5 @@
-
-
 import streamlit as st
+import streamlit.components.v1 as components
 
 # Título da aplicação
 st.title('Dashboard Interativo do Power BI')
@@ -12,4 +11,4 @@ st.write('Bem-vindo ao dashboard interativo!')
 power_bi_url = 'https://app.powerbi.com/view?r=eyJrIjoiY2JlZWRlYmYtY2VkYy00OTI1LWJjODYtY2UwODhhMTAzZTJjIiwidCI6IjZkNDg1YWY3LTAxZjYtNGE2MC05OWEwLTQzOTQyNzJiMjUzNSJ9'
 
 # Exibindo o relatório do Power BI de forma interativa
-st.components.v1.iframe(power_bi_url, width=1200, height=800, scrolling=True)
+components.iframe(power_bi_url, width=1200, height=800, scrolling=True)
