38cf5793eb7a6100f0a8e50f04e63ff7f3d8c893
sotnasnonnel <90616389+sotnasnonnel@users.noreply.github.com>
2024-05-17 17:10:47 -0300

    Add files via upload

---
+++ dashbi.py
@@ -0,0 +1,15 @@
+
+
+import streamlit as st
+
+# Título da aplicação
+st.title('Dashboard Interativo do Power BI')
+
+# Texto de introdução
+st.write('Bem-vindo ao dashboard interativo!')
+
+# URL de incorporação do Power BI
+power_bi_url = 'https://app.powerbi.com/view?r=eyJrIjoiY2JlZWRlYmYtY2VkYy00OTI1LWJjODYtY2UwODhhMTAzZTJjIiwidCI6IjZkNDg1YWY3LTAxZjYtNGE2MC05OWEwLTQzOTQyNzJiMjUzNSJ9'
+
+# Exibindo o relatório do Power BI de forma interativa
+st.components.v1.iframe(power_bi_url, width=1200, height=800, scrolling=True)
