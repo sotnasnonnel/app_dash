bf4da20acbebc61218ccc5455decedb724b18fdd
sotnasnonnel <90616389+sotnasnonnel@users.noreply.github.com>
2024-05-18 11:08:07 -0300

    Update dash power BI.py

--- dash power BI.py
+++ dash power BI.py
@@ -1,15 +1,61 @@
-
-
 import streamlit as st
+import streamlit.components.v1 as components
+from streamlit_option_menu import option_menu
 
-# Título da aplicação
-st.title('Dashboard Interativo do Power BI')
+# Configurar a página para o layout 'wide'
+st.set_page_config(layout="wide")
 
-# Texto de introdução
-st.write('Bem-vindo ao dashboard interativo!')
+# Estilizar a barra lateral
+st.markdown(
+"""
+<style>
+.css-1lcbmhc.e1fqkh3o3 {
+    background-color: #3e5265;
+    color: #f2f2f2;
+}
+.css-1lcbmhc.e1fqkh3o3:hover {
+    background-color: #3e5265;
+    color: #bdcdd8;
+}
+</style>
+""", unsafe_allow_html=True)
 
-# URL de incorporação do Power BI
-power_bi_url = 'https://app.powerbi.com/view?r=eyJrIjoiY2JlZWRlYmYtY2VkYy00OTI1LWJjODYtY2UwODhhMTAzZTJjIiwidCI6IjZkNDg1YWY3LTAxZjYtNGE2MC05OWEwLTQzOTQyNzJiMjUzNSJ9'
+with st.sidebar:
+    st.title("Dashboard")
 
-# Exibindo o relatório do Power BI de forma interativa
-st.components.v1.iframe(power_bi_url, width=1200, height=800, scrolling=True)
+
+# Exibir um exemplo da cor selecionada
+st.markdown(
+    f'<div style="background-color:s#f2f2f;padding:10px;border-radius:5px;"></div>',
+    unsafe_allow_html=True
+)
+
+# Função para exibir cada relatório
+def show_report(url):
+    components.iframe(url, width=1200, height=750, scrolling=True)
+
+# Lista de relatórios com título e URL
+reports = {
+    "Relatório 1": "https://app.powerbi.com/view?r=eyJrIjoiZWFiN2VlZjQtZmNkMS00YmJkLWI1MzItYjYxNDMyYjdiZjY1IiwidCI6IjZkNDg1YWY3LTAxZjYtNGE2MC05OWEwLTQzOTQyNzJiMjUzNSJ9",
+    "Relatório 2": "https://app.powerbi.com/view?r=eyJrIjoiNWE0NjY3MjEtMTdkNi00YjAwLTg3ZjgtNmM1ZDhhMzY1MGU0IiwidCI6IjZkNDg1YWY3LTAxZjYtNGE2MC05OWEwLTQzOTQyNzJiMjUzNSJ9",
+    "Relatório 3": "https://app.powerbi.com/view?r=eyJrIjoiNDhmZmRhNzMtYzBiMS00ZjRmLThiMGUtYjc3NzkyNGRlODIwIiwidCI6IjZkNDg1YWY3LTAxZjYtNGE2MC05OWEwLTQzOTQyNzJiMjUzNSJ9",
+    "Relatório 4": "https://app.powerbi.com/view?r=eyJrIjoiMzBiMWI3MjQtYzQ0Zi00OWI0LTliY2ItNTAyNmQ4NzY3MzVkIiwidCI6IjZkNDg1YWY3LTAxZjYtNGE2MC05OWEwLTQzOTQyNzJiMjUzNSJ9",
+    "Relatório 5": "https://app.powerbi.com/view?r=eyJrIjoiYmY3NWIyNGMtNDdlYy00Y2JkLWEzYzgtY2Q5MWY3Y2Q1NDcxIiwidCI6IjZkNDg1YWY3LTAxZjYtNGE2MC05OWEwLTQzOTQyNzJiMjUzNSJ9"
+}
+
+# Menu lateral para seleção de relatórios
+with st.sidebar:
+    # Adiciona logo (certifique-se de que o caminho para a logo está correto)
+    st.image("caminho_para_logo.png", use_column_width=True)
+    
+    st.title("Navegação")
+    selected = option_menu(
+        menu_title=None,  # menu title
+        options=list(reports.keys()),  # options
+        icons=["bar-chart-fill"]*5,  # icons
+        menu_icon="cast",  # menu icon
+        default_index=0,  # default selected index
+    )
+
+# Exibe o relatório selecionado
+show_report(reports[selected])
